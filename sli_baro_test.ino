020e7e020d40717df95e97665d5286ab0763e1be
stefandebruyn <stefandebruyn@utexas.edu>
2019-10-03 09:59:09 -0500

    Finishing touches

--- sli_baro_test.ino
+++ sli_baro_test.ino
@@ -1,7 +1,8 @@
 // #define HEADLESS_TEST
+// #define SERIAL_DEBUG
 #define TELEMETRY_FNAME "tlm.dat" // Be careful changing this
-#define TELEMETRY_DURATION 2 // Seconds
-#define TELEMETRY_FREQUENCY 20 // Hz
+#define TELEMETRY_DURATION 10 * 60 // Seconds to record for
+#define TELEMETRY_FREQUENCY 10 // Hz of readings
 
 #include <SD.h>
 
@@ -30,30 +31,44 @@ void setup() {
   while (!Serial);
 
   // Connect to BMP085
+#ifdef SERIAL_DEBUG
   Serial.println("Initializing barometer...");
+#endif
   bool status = g_baro.begin(BMP085_ULTRALOWPOWER);
 
 #ifndef HEADLESS_TEST
   if (!status) {
+  #ifdef SERIAL_DEBUG
     Serial.println("FATAL: FAILED TO CONTACT BMP085; ABORTING STARTUP");
+  #endif
     return;
   }
 #endif
 
   // Initialize SD IO
+#ifdef SERIAL_DEBUG
   Serial.println("Initializing SD card...");
+#endif
   status = SD.begin(BUILTIN_SDCARD);
 
   if (!status) {
+  #ifdef SERIAL_DEBUG
     Serial.println("FATAL: FAILED TO CONTACT SD CARD; ABORTING STARTUP");
+  #endif
     return;
   }
+
+  // Clear old file
+  if (SD.exists(TELEMETRY_FNAME))
+    SD.remove(TELEMETRY_FNAME);
 
   g_epoch_ms = millis();
   g_lastrec_t = 0;
   g_setup_success = true;
 
+#ifdef SERIAL_DEBUG
   Serial.println("Startup successful");
+#endif
 }
 
 void loop() {
@@ -71,11 +86,11 @@ void loop() {
     return;
 
   // Read telemetry
-  float pressure = 9.81; // g_baro.readPressure();
-  float temperature = 3.14159; // g_baro.readTemperature();
+  float pressure = g_baro.readPressure();
+  float temperature = g_baro.readTemperature();
 
   // Pack telemetry into buffer. Each telemetry vector <time, pressure, temp>
-  // is recorded in 12 contiguous Big Endian bytes.
+  // is appended to the datfile as 12 bytes in Big Endian.
   static const int BUFFER_LEN = 12;
   char buffer[BUFFER_LEN];
   FloatBytes bytes;
@@ -107,7 +122,9 @@ void loop() {
   File out = SD.open(TELEMETRY_FNAME, FILE_WRITE);
   out.write(buffer, BUFFER_LEN);
   out.close();
-  Serial.println("wrote");
+#ifdef SERIAL_DEBUG
+  Serial.println("Wrote telemetry vector");
+#endif
 
   g_lastrec_t = now;
 }
